f9547b7d3474b4ec32f1edf0171ec5272beff832
Skye <118846397+NobleSkye@users.noreply.github.com>
2025-04-17 22:02:34 +0000

    changed logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { siteConfig } from '../config/config';
 import SkillBar from '../components/SkillBar';
-import { Github, GithubIcon, Mail, MapPin, Youtube } from 'lucide-react';
+import { Github, Mail, MapPin } from 'lucide-react';
 import CertList from '../components/CertList';
 
 export default function Home() {
@@ -29,7 +29,7 @@ export default function Home() {
                 </a>
               </div>
               <div className="flex items-center gap-2">
-                <Github className="w-5 h-5 text-gray-400" />
+                <square-dashed-bottom-code className="w-5 h-5 text-gray-400" />
                 <a href={`https://github.com/${siteConfig.social.github.name}`} className="text-gray-300 hover:text-blue-400">
                   {siteConfig.contact.github}
                 </a>
